01610f954d74b2f2e89678d2bd29282c2cfd6cd7
“Felipe1401” <feliubkn@gmail.com>
2023-12-04 17:05:44 -0300

    reorganize

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='JeriPy',
-    packages=["JeriPy"],
+    packages=find_packages(),
     version='0.1.0',
     author='Felipe Gomez',
     author_email='felipenicolasgomezmolina@gmail.com',
